fc7ed8bdf6133fb8912e8d3926e43d369449dd1e
Alonso Ortega <alonsojortega@gmail.com>
2017-08-24 00:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name){
-  kittens.push('kitten')
+  kittens.push('name')
   return kittens;
 }
